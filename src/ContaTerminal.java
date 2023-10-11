1cb6766fd0c15b42b403621831c2e8715616eecc
PSouza95 <pedro_drakon@hotmail.com>
2023-10-11 18:49:22 -0300

    Alterando o README.md

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //Conhecer e importar a classe Scanner
+
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        //Exibir as mensagens para o nosso usuario
+
         System.out.println("Por favor, digite o número da conta !");
         int numero = scanner.nextInt();
 
@@ -18,8 +18,8 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o valor do saldo !");
         double saldo = scanner.nextDouble();
-        //Obter pelo Scanner os valores digitados no terminal
-        //Exibir a mensagem conta criada
+
+
         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero +" e seu saldo " + saldo + " já está disponível para saque.");
 
         scanner.close();
